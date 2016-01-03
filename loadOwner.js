f71ddff8f151f87dc780fd34c2fa0d756965582b
Jon Graf <jongraf1@gmail.com>
2016-01-03 16:43:35 -0600

    Loading Contact information and clearing out unused elements on different tabs.

--- loadOwner.js
+++ loadOwner.js
@@ -34,3 +34,55 @@ function loadOwnerJSON() {
         });
 	}); 
 }
+
+function loadMailJSON() {
+        $(document).ready(function() {
+        jQuery.ajax({
+            url: "OwnerJson.json",
+            dataType:'json',
+            success:function(response) {
+                $('#header1').html(response.Contact.firstHeader);
+                $('#paragraph1').html(response.Contact.firstP1);
+                $('#lineBreaks1').css("visibility","visible");
+                $('#paragraph2').html(response.Contact.secondP1);
+                $('#paragraph3').html(response.Contact.thirdP1);
+                $('#paragraph4').html(response.Contact.fourthP1);
+
+                // clean up divs
+                $('#header4').html("");
+                $('#paragraph5').html("");
+                $('#paragraph6').html("");
+
+                //remove elements
+                // $('#header2').detach();
+                // $('#header3').detach();
+                // $('#header4').detach();
+            }
+        });
+    }); 
+}
+
+function loadEmailJSON() {
+            $(document).ready(function() {
+        jQuery.ajax({
+            url: "OwnerJson.json",
+            dataType:'json',
+            success:function(response) {
+                $('#header1').html(response.Contact.firstHeader);
+                $('#paragraph1').html(response.Contact.firstP1);
+                $('#lineBreaks1').css("visibility","visible");
+                $('#paragraph2').html(response.Contact.firstP1Email);
+
+                // clean up divs
+                $('#paragraph3').html("");
+                $('#paragraph4').html("");
+                $('#paragraph5').html("");
+                $('#paragraph6').html("");
+                $('#header3').html("");
+                $('#header4').html("");
+                $('#header5').html("");
+                $('#header6').html("");
+            }
+        });
+    });
+}
