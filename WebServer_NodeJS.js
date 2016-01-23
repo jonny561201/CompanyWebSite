537b98edf154208f086dcc4f414c4f10bf4db011
Jon Graf <jongraf1@gmail.com>
2016-01-23 01:47:11 -0600

    Simplified Node Server to handle generic value types and pass pack objects!

--- WebServer_NodeJS.js
+++ WebServer_NodeJS.js
@@ -1,9 +1,22 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 var dispatcher = require('httpdispatcher');
 
 const PORT = 8080;
 var server;
+var fileName;
+var validExtensions = {
+	".html":"text/html",
+	".png":"image/png",
+	".jpg":"image/jpg",
+	".js":"application/javascript",
+	".otf":"font/opentype",
+	".css":"text/css",
+	".txt":"text/plain",
+	".json":"application/json"
+};
+
 dispatcher.setStatic('WebSite');
 
 server = http.createServer(requestHandler);
@@ -13,112 +26,19 @@ server.listen(PORT, function(){console.log("Server listening on Port: " + PORT)}
 function requestHandler(request, response){
 
 	try{
+		fileName = request.url;
 		console.log(request.url);
-		dispatcher.dispatch(request, response);
+		if (fileName == "/") {
+			fileName = "/index.html"
+		}
+
+		var ext = path.extname(fileName);
+		
+		var isValidExtension = validExtensions[ext];
+		var loadImage = fs.readFileSync("." + fileName);
+		response.writeHead(200, {"Content-Type":isValidExtension});
+		response.end(loadImage, 'binary');
 	} catch(err){
 		console.log(err);
 	}
 }
-
-dispatcher.onGet("/", function(req, res){
-	var htmlPage = fs.readFileSync('./index.html');
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write(htmlPage);
-	res.end();
-});
-
-dispatcher.onGet("/index.css", function(req, res){
-	var cssPage = fs.readFileSync('./index.css');
-	res.writeHead(200, {'Content-Type': 'text/css'});
-	res.write(cssPage);
-	res.end();
-});
-
-dispatcher.onGet("/Owner.txt", function(req, res){
-	var Owner = fs.readFileSync('./Owner.txt');
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write(Owner);
-	res.end();
-});
-
-dispatcher.onGet("/loadOwner.js", function(req, res){
-	var jscript = fs.readFileSync('./loadOwner.js');
-	res.writeHead(200, {'Content-Type': 'application/javascript'});
-	res.write(jscript);
-	res.end();
-});
-
-dispatcher.onGet("/js/jquery-1.11.0.min.js", function(req, res){
-	var jscript = fs.readFileSync('./js/jquery-1.11.0.min.js');
-	res.writeHead(200, {'Content-Type': 'application/javascript'});
-	res.write(jscript);
-	res.end();
-});
-
-dispatcher.onGet("/js/bootstrap.min.js", function(req, res){
-	var jscript = fs.readFileSync('./js/bootstrap.min.js');
-	res.writeHead(200, {'Content-Type': 'application/javascript'});
-	res.write(jscript);
-	res.end();
-});
-
-dispatcher.onGet("/Sky-Blue-Sky.jpg", function(req , res){
-	var backImg = fs.readFileSync('./Sky-Blue-Sky.jpg');
-	res.writeHead(200, {'Content-Type': 'image/jpg'});
-	res.end(backImg, 'binary');
-});
-
-dispatcher.onGet("/lineBreaks.png", function(req , res){
-	var backImg = fs.readFileSync('./lineBreaks.png');
-	res.writeHead(200, {'Content-Type': 'image/png'});
-	res.end(backImg, 'binary');
-});
-
-dispatcher.onGet("/logo.png", function(req, res){
-	var logo = fs.readFileSync('./logo.png');
-	res.writeHead(200, {'Content-Type': 'image/png'});
-	res.end(logo, 'binary');
-});
-
-dispatcher.onGet("/CEO-Photo.png", function(req, res){
-	var ceoImg = fs.readFileSync('./CEO-Photo.png');
-	res.writeHead(200, {'Content-Type': 'image/png'});
-	res.end(ceoImg, 'binary');
-});
-
-dispatcher.onGet("/linkedin_icon.png", function(req, res){
-	var linkedInImg = fs.readFileSync('./linkedin_icon.png');
-	res.writeHead(200, {'Content-Type': 'image/png'});
-	res.end(linkedInImg, 'binary');
-});
-
-dispatcher.onGet("/email_icon.png", function(req, res){
-	var mailImg = fs.readFileSync('./email_icon.png');
-	res.writeHead(200, {'Content-Type': 'image/png'});
-	res.end(mailImg, 'binary');
-});
-
-dispatcher.onGet("/github_icon.png", function(req, res){
-	var githubImg = fs.readFileSync('./github_icon.png');
-	res.writeHead(200, {'Content-Type': 'image/png'});
-	res.end(githubImg, 'binary');
-});
-
-dispatcher.onGet("/Condiment-Regular.otf", function(req, res){
-	var font = fs.readFileSync('./Condiment-Regular.otf');
-	res.writeHead(200, {'Content-Type': 'font/opentype'});
-	res.end(font, 'binary');
-});
-
-dispatcher.onGet("/OwnerJson.json", function(req, res){
-	var json = fs.readFileSync('./OwnerJson.json');
-	res.writeHead(200, {'Content-Type': 'application/json'});
-	res.write(json);
-	res.end();
-});
-
-// dispatcher.onGet("/image", function(req, res){
-// 	var background = fs.readFileSync('./Original.png');
-// 	res.writeHead(200, {'Content-Type': 'image/png'});
-// 	res.end(background, 'binary');
-// })
