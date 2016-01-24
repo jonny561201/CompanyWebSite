d43b78b57c068eda8361a96ef1671ce0d1b0428b
Jon Graf <jongraf1@gmail.com>
2016-01-23 19:40:46 -0600

    Buttons correctly highlight using JS and simplified JS file for implicit Jquery iterations

--- loadOwner.js
+++ loadOwner.js
@@ -5,6 +5,7 @@ function loadOwnerJSON() {
             url: "OwnerJson.json",
             dataType:'json',
             success:function(response) {
+                highlightButtons('#aboutButton');
                 clearText();
                 insertImage('CEO-Photo.png');
                 $('#header1').html(response.OwnerPage.firstHeader);
@@ -32,19 +33,9 @@ function loadNewsJSON() {
             url: "OwnerJson.json",
             dataType:'json',
             success:function(response) {
+                highlightButtons('#homeButton');
                 clearText();
-            }
-        });
-    });
-}
-
-function loadUpdatesJSON() {
-    $(document).ready(function() {
-        jQuery.ajax({
-            url: "OwnerJson.json",
-            dataType:'json',
-            success:function(response) {
-                clearText();
+                
             }
         });
     });
@@ -56,6 +47,7 @@ function loadMailJSON() {
             url: "OwnerJson.json",
             dataType:'json',
             success:function(response) {
+                highlightButtons('#contactButton');
                 clearText();
                 $('#header1').html(response.Contact.firstHeader);
                 $('#paragraph1').html(response.Contact.firstP1);
@@ -78,39 +70,13 @@ function loadMailJSON() {
     }); 
 }
 
-function loadEmailJSON() {
-    $(document).ready(function() {
-        jQuery.ajax({
-            url: "OwnerJson.json",
-            dataType:'json',
-            success:function(response) {
-                clearText();
-                $('#header1').html(response.Contact.firstHeader);
-                $('#paragraph1').html(response.Contact.firstP1);
-                $('#lineBreaks1').css("visibility","visible");
-                $('#paragraph2').html(response.Contact.firstP1Email);
-                $('#paragraph2').css("href","mailto:jongraf1@gmail.com?Subject=Soaring%20Leaf%20Solutions,%20LLC");
-
-                //remove elements
-                $('#header2').css("margin","0px");
-                $('#header3').css("margin","0px");
-                $('#header4').css("margin","0px");
-                $('#header5').css("margin","0px");
-                $('#lineBreaks2').css("height","0px");
-                $('#lineBreaks3').css("height","0px");
-                $('#lineBreaks4').css("height","0px");
-                $('#lineBreaks5').css("height","0px");
-            }
-        });
-    });
-}
-
 function loadHistoryJSON() {
     $(document).ready(function() {
         jQuery.ajax({
             url: "OwnerJson.json",
             dataType: 'json',
             success:function(response) {
+                highlightButtons('#historyButton');
                 clearText();
                 $('#header1').html(response.History.firstHeader);
                 $('#lineBreaks1').css("visibility","visible");
@@ -124,6 +90,11 @@ function loadHistoryJSON() {
             }
         });
     });
+}
+
+function highlightButtons(buttonName) {
+    $('button').css({"background" : "transparent"});
+    $(buttonName).css({"background-color":"#0099ff"});
 }
 
 function insertImage(imgSrc) {
@@ -164,38 +135,9 @@ function clearText() {
         $('#CEOTitle').remove();
         $('.socialMediaList').remove();
     }
-    $('#paragraph1').html("");
-    $('#paragraph2').html("");
-    $('#paragraph3').html("");
-    $('#paragraph4').html("");
-    $('#paragraph5').html("");
-    $('#paragraph6').html("");
-    $('#paragraph7').html("");
-    $('#lineBreaks1').css("visibility","hidden");
-    $('#lineBreaks2').css("visibility","hidden");
-    $('#lineBreaks3').css("visibility","hidden");
-    $('#lineBreaks4').css("visibility","hidden");
-    $('#lineBreaks5').css("visibility","hidden");
-    $('#lineBreaks6').css("visibility","hidden");
-    $('#lineBreaks7').css("visibility","hidden");
+    $('.insesrtText').html("");
+    $('.breaks').css({"visibility" : "hidden", "height" : "8px"});
     $('#header1').css({"margin-top" : "10px", "padding-top" : "7%"}); 
-    $('#header2').css("margin-top","10px");  
-    $('#header2').css("margin-bottom","10px"); 
-    $('#header3').css("margin-top","10px");  
-    $('#header3').css("margin-bottom","10px"); 
-    $('#header4').css("margin-top","10px");  
-    $('#header4').css("margin-bottom","10px"); 
-    $('#header5').css("margin-top","10px");  
-    $('#header5').css("margin-bottom","10px"); 
-    $('#lineBreaks2').css("height","8px");
-    $('#lineBreaks3').css("height","8px");
-    $('#lineBreaks4').css("height","8px");
-    $('#lineBreaks5').css("height","8px");
-    $('#header1').html("");
-    $('#header2').html("");
-    $('#header3').html("");
-    $('#header4').html("");
-    $('#header5').html("");
-    $('#header6').html("");
-    $('#header7').html("");
+    $().css({"margin-top" : "10px", "margin-bottom" : "10px"});
+    $('.headers').html("");
 }
