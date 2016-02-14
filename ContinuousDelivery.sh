cb1dd78dd5b00216674da500fcf6fbc80c1512ea
Jon Graf <jongraf1@gmail.com>
2016-02-14 11:52:01 -0600

    updated shell script to incorporate pauses to debug on server

--- ContinuousDelivery.sh
+++ ContinuousDelivery.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 cd C:/Users/Inuyasha/Documents/WebSite
 
-
+read -n 1 -s
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse @{u})
 BASE=$(git merge-base @ @{u})
 
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
+    read -n 1 -s
 elif [ $LOCAL = $BASE ]; then
 	taskkill -im node.exe -f
 	git pull -r
 	start C:/Users/Inuyasha/Desktop/StartWebServer.bat
+	read -n 1 -s
 fi
