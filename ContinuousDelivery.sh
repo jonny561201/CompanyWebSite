1901aaa6263dee2880e8ebab24b9fd8f91659f50
jonny561201 <jongraf1@gmail.com>
2016-02-14 12:51:30 -0600

    added ignore file to exclude node modules from project

--- ContinuousDelivery.sh
+++ ContinuousDelivery.sh
@@ -1,17 +1,36 @@
 #!/bin/sh
-cd C:/Users/Inuyasha/Documents/WebSite
+
 
 read -n 1 -s
+
+cd C:/Users/Shippo/Documents/SoaringLeafWebsite
+
+
+
 LOCAL=$(git rev-parse @)
+
 REMOTE=$(git rev-parse @{u})
+
 BASE=$(git merge-base @ @{u})
 
+
+
 if [ $LOCAL = $REMOTE ]; then
+
+	echo "Local: "$LOCAL
+	echo "Remote: " $REMOTE
     echo "Up-to-date"
+
     read -n 1 -s
+
 elif [ $LOCAL = $BASE ]; then
+
 	taskkill -im node.exe -f
+
 	git pull -r
-	start C:/Users/Inuyasha/Desktop/StartWebServer.bat
+
+	start C:/Users/Shippo/Desktop/StartWebServer.bat
+
 	read -n 1 -s
+
 fi
