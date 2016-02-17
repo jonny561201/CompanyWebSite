150d20396d7caf12e4c34401bda2d878c865ff48
jonny561201 <jongraf1@gmail.com>
2016-02-16 19:29:12 -0600

    updated conintuous delivery shell to now contain fetch to fix issue with not getting latest hash

--- ContinuousDelivery.sh
+++ ContinuousDelivery.sh
@@ -1,36 +1,23 @@
 #!/bin/sh
 
 
-read -n 1 -s
-
 cd C:/Users/Shippo/Documents/SoaringLeafWebsite
-
+git fetch
 
 
 LOCAL=$(git rev-parse @)
-
 REMOTE=$(git rev-parse @{u})
-
 BASE=$(git merge-base @ @{u})
 
 
 
 if [ $LOCAL = $REMOTE ]; then
-
 	echo "Local: "$LOCAL
 	echo "Remote: " $REMOTE
-    echo "Up-to-date"
-
-    read -n 1 -s
-
+	echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
-
 	taskkill -im node.exe -f
-
 	git pull -r
-
 	start C:/Users/Shippo/Desktop/StartWebServer.bat
-
 	read -n 1 -s
-
 fi
